de8440ae81fe6802782c522d736abcaee00e0366
Tony L <99093620+justapotato213@users.noreply.github.com>
2022-09-29 13:13:51 -0400

    feat: add lab 04 (basic javascript)

---
+++ javascript/lab04.js
@@ -0,0 +1,36 @@
+/*
+  Name:        New File Generator (script.js)
+  Purpose:     Generates a new file for use in the ICS3U course
+
+  Author:      Tony Lin 708162
+  Created:     28-Sep-2022
+  Updated:     28-Sep-2022
+*/
+
+// LESSON CODE BELOW ------->
+//Code goes here
+//name of variable  =  information
+// let name = "Duck"     //Words/Text -> String Data
+// let age = 16        //numerical Data
+// let had_lunch = false      //Boolean Data (T/F)
+
+// console.log("Hello " + name)
+// console.log("The " + name + "'s age is " + age)
+// console.log("Did the " + name + " have lunch? " + had_lunch)
+// LESSON CODE FINISHED ------>
+
+// TASK CODE BELOW -------->
+// setting default numbers
+let num1 = 123
+let num2 = 213
+//  processing the numbers
+let numAddition = num1 + num2
+let numProduct = num1 * num2
+let numRemainder = num2 % num1
+// outputting the equations
+console.log("Numbers added together " + numAddition)
+console.log("Numbers multiplied together " + numProduct)
+console.log("Numbers divided " + num2 / num1)
+console.log("Numbers subtracted " + (num2 - num1))
+console.log("Numbers remainder " + numRemainder)
+// TASK CODE FINISHED ----->
