6846d28f9ac87a1f1efcd196922ce9fbee920c37
Tony L <99093620+justapotato213@users.noreply.github.com>
2022-09-30 13:57:30 -0400

    Create lab04a.js

---
+++ javascript/lab04a.js
@@ -0,0 +1,38 @@
+/*
+  Name:        New File Generator (script.js)
+  Purpose:     Generates a new file for use in the ICS3U course
+
+  Author:      Tony Lin 708162
+  Created:     29-Sep-2022
+  Updated:     30-Sep-2022
+*/
+
+// LESSON CODE BELOW ------->
+//Code goes here
+//name of variable  =  information
+// let name = "Duck"     //Words/Text -> String Data
+// let age = 16        //numerical Data
+// let had_lunch = false      //Boolean Data (T/F)
+
+// console.log("Hello " + name)
+// console.log("The " + name + "'s age is " + age)
+// console.log("Did the " + name + " have lunch? " + had_lunch)
+// LESSON CODE FINISHED ------>
+
+// TASK CODE BELOW -------->
+// setting default numbers
+let num1 = parseInt(prompt("Enter the first number: "))
+let num2 = parseInt(prompt("Enter the second number: "))
+//  processing the numbers
+let numAddition = num1 + num2
+let numProduct = num1 * num2
+let numRemainder = num2 % num1
+// outputting the equations
+console.log("Numbers added together " + numAddition)
+console.log("Numbers multiplied together " + numProduct)
+console.log("Numbers divided " + num2 / num1)
+console.log("Numbers subtracted " + (num2 - num1))
+console.log("Numbers remainder " + numRemainder)
+const password = "asoidjfaowjeroiajweoirj"
+console.log(password + "6")
+// TASK CODE FINSIHED --------->
